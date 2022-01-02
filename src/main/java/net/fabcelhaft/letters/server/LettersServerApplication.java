3dd2c3806c7a8dcae7924d9d3a9a22a96273c8d9
Kikkirej <5462557+Kikkirej@users.noreply.github.com>
2022-01-02 19:33:09 +0100

    start.spring.io generated project

---
+++ src/main/java/net/fabcelhaft/letters/server/LettersServerApplication.java
@@ -0,0 +1,13 @@
+package net.fabcelhaft.letters.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettersServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LettersServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/fabcelhaft/letters/server/LettersServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.fabcelhaft.letters.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LettersServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
