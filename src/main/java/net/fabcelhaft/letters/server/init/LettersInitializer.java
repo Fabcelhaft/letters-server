212df741a90e5f6a1572052aaca56bab2f08d57a
Aitareko <77382573+Aitareko@users.noreply.github.com>
2022-01-02 21:13:08 +0100

    🚧 implement user creation

--- src/main/java/net/fabcelhaft/letters/server/init/LettersInitializer.java
+++ src/main/java/net/fabcelhaft/letters/server/init/LettersInitializer.java
@@ -1,10 +1,14 @@
 package net.fabcelhaft.letters.server.init;
 
+import net.fabcelhaft.letters.server.model.User;
+import net.fabcelhaft.letters.server.security.ProtectedKeyPair;
 import net.fabcelhaft.letters.server.security.UserCryptoGenerator;
 import net.fabcelhaft.letters.server.security.UserPasswordGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class LettersInitializer {
@@ -19,8 +23,20 @@ public class LettersInitializer {
     }
 
     @Async
+    @Transactional
     public void initializeUsers() {
+        User user = new User();
+        String userPassword = userPasswordGenerator.getGeneratedPassword();
 
+        user.setSalt(userPasswordGenerator.getGeneratedSalt());
+        setCryptoToUser(user, userPassword);
+
+    }
+
+    private void setCryptoToUser(User user, String password) {
+        ProtectedKeyPair protectedKeyPair = userCryptoGenerator.generateCrypto(password, user.getSalt());
+        user.setPublicKey(protectedKeyPair.getPublicKey());
+        user.setPrivateKey(protectedKeyPair.getPrivateKey());
     }
 
 }
