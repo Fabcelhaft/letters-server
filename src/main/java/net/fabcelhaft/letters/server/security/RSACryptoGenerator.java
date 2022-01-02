59679523c368795f7bbe73bf1714e88ccbf1f354
Aitareko <77382573+Aitareko@users.noreply.github.com>
2022-01-02 23:54:57 +0100

    🔒 switch crypto to RSA-4096

--- src/main/java/net/fabcelhaft/letters/server/security/RSACryptoGenerator.java
+++ src/main/java/net/fabcelhaft/letters/server/security/RSACryptoGenerator.java
@@ -7,21 +7,21 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 
-@Service
 @Slf4j
-public class Ed25519CryptoGenerator implements UserCryptoGenerator{
+@Service
+public class RSACryptoGenerator implements UserCryptoGenerator {
 
     @Override
     public ProtectedKeyPair generateCrypto(String password, String salt) {
         try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("Ed25519");
-            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+            generator.initialize(4096);
+            KeyPair keyPair = generator.generateKeyPair();
             return new ProtectedKeyPair(keyPair, password, salt);
         } catch (NoSuchAlgorithmException noSuchAlgorithmException) {
             log.error("Error generating KeyPair. User can't be created!", noSuchAlgorithmException);
             throw new IllegalStateException("Error generating KeyPair. User can't be created!");
         }
-
     }
 
 }
