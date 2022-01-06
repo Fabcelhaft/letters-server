13957f4668137fb1d15180e9af4abe2acc31dbd5
Aitareko <77382573+Aitareko@users.noreply.github.com>
2022-01-06 19:59:08 +0100

    🗃 change findAll() to return type Set

--- src/main/java/net/fabcelhaft/letters/server/repository/UserRepository.java
+++ src/main/java/net/fabcelhaft/letters/server/repository/UserRepository.java
@@ -3,6 +3,11 @@ package net.fabcelhaft.letters.server.repository;
 import net.fabcelhaft.letters.server.model.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import java.util.Set;
+
+public interface UserRepository extends CrudRepository<User, String> {
+
+    Set<User> findAll();
+
     User findUserByMail(String mail);
 }
